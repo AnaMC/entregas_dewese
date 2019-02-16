03c639a2227b069258d14443a41186f879cb527e
Ana MC <troglodita91@gmail.com>
2019-02-16 17:58:54 +0000

    Proyecto links favoritos

---
+++ links_favoritos/classes/izv/mvc/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace izv\mvc;
+
+class Router {
+
+    private $rutas, $ruta;
+    
+    function __construct($ruta) {
+        $this->rutas = array(
+            'admin' => new Route('AdminModel', 'AdminView' , 'AdminController'),
+            'index' => new Route('UserModel', 'MaundyView', 'UserController'),
+            'ajax' => new Route('UserModel', 'AjaxView', 'AjaxController')
+            //'old'   => new Route('FirstModel', 'FirstView' , 'FirstController'),
+            //'zeta'  => new Route('FirstModel', 'SecondView', 'FirstController')
+        );
+        $this->ruta = $ruta;
+    }
+
+    function getRoute() {
+        $ruta = $this->rutas['index'];
+        if(isset($this->rutas[$this->ruta])) {
+            $ruta = $this->rutas[$this->ruta];
+        }
+        return $ruta;
+    }
+}
